89d42314676ebe9e01e36b6b7340cfc3941d0488
Lucas Ellenberger <lmellenb@ucsc.edu>
2024-04-11 23:24:49 -0700

    Pass through test 6.

--- Project1/rbf/pfm.cc
+++ Project1/rbf/pfm.cc
@@ -3,6 +3,12 @@
 
 #include "pfm.h"
 
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::endl;
+
+
 PagedFileManager *PagedFileManager::_pf_manager = 0;
 
 PagedFileManager *PagedFileManager::instance()
@@ -27,14 +33,14 @@ RC PagedFileManager::createFile(const string &fileName)
     struct stat fileAtt;
     if (stat(fileName.c_str(), &fileAtt) == 0)
     {
-        std::cerr << "File already Exists\n";
+        cerr << "File already Exists\n";
         return 1;
     }
 
     FILE *file = fopen(fileName.c_str(), "wb");
     if (file == NULL)
     {
-        std::cerr << "Error in creating file\n";
+        cerr << "Error in creating file\n";
         return 2;
     }
     fclose(file);
@@ -47,12 +53,12 @@ RC PagedFileManager::destroyFile(const string &fileName)
     struct stat fileAtt;
     if (stat(fileName.c_str(), &fileAtt) != 0)
     {
-        std::cerr << "Error obtaining file\n";
+        cerr << "Error obtaining file\n";
         return 1;
     }
     if (remove(fileName.c_str()) != 0)
     {
-        std::cerr << "Error in removing file\n";
+        cerr << "Error in removing file\n";
         return 2;
     }
     return 0;
@@ -68,7 +74,7 @@ RC PagedFileManager::openFile(const string &fileName, FileHandle &fileHandle)
             std::cerr << "File does not exist\n";
             return 1;
         }
-        std::cerr << "Error Occured\n";
+        cerr << "Error Occured\n";
         return 2;
     };
 
@@ -91,7 +97,7 @@ RC PagedFileManager::closeFile(FileHandle &fileHandle)
 {
     if (fileHandle.file == nullptr)
     {
-        std::cerr << "fileHandle object does not have an open file\n";
+        cerr << "fileHandle object does not have an open file\n";
         return 1;
     }
     fclose(fileHandle.file);
@@ -110,21 +116,63 @@ FileHandle::FileHandle()
 
 FileHandle::~FileHandle()
 {
+    // need to destroy any files held by pointers
+    if(this->file != nullptr) {
+        //has file associated with it and file is still open. 
+        cerr << "File not closed, cannot remove file handle" << endl;
+    }
 }
 
 RC FileHandle::readPage(PageNum pageNum, void *data)
 {
-    return -1;
+    //given the page num, modify the memory block at *data to have all the information stored the page pageNum
+    if (this->pagecount <= pageNum) {
+        cerr << "page number requested is greater than the number of pages for this file" << endl;
+        return -1;
+    }
+
+    //given that any page is 4096 bytes, then we should be able to just say "page 0 starts at byte 0 ends at 4095, page 1 starts at 4096... "
+    unsigned offset = pageNum << 12; //multiply pageNum by 4096 to get to correct page
+
+    fseek(this->file, offset, SEEK_SET); // set us to the correct spot in the file for the requested page
+    fread(data, 1, 4096, this->file); //read the data
+
+
+    this->readPageCounter += 1;
+    return 0;
 }
 
 RC FileHandle::writePage(PageNum pageNum, const void *data)
 {
-    return -1;
+    //given the page num, modify the page to have all the data pointed to by data
+    if (pageNum > this->pagecount) {
+        cerr << "page number requested is greater than the number of pages for this file" << endl;
+        return -1;
+    }
+    if (pageNum == this->pagecount) {
+        this->writePageCounter += 1;
+        return appendPage(data);
+    }
+
+    unsigned offset = pageNum << 12; //multiply pageNum by 4096 to get to correct page
+
+    fseek(this->file, offset, SEEK_SET); // set us to the correct spot in the file for the requested page
+    fwrite(data, 1, 4096, this->file); //write the data
+
+    this->writePageCounter += 1;
+    return 0;
 }
 
 RC FileHandle::appendPage(const void *data)
 {
-    return -1;
+    unsigned offset = this->pagecount << 12; //multiply number of pages by 4096 to get to correct page
+
+    fseek(this->file, offset, SEEK_SET); // set us to the correct spot in the file for the requested page
+    fwrite(data, 1, 4096, this->file); //write the data
+
+    this->appendPageCounter += 1;
+    this->pagecount += 1;
+    return 0;
 }
 
 unsigned FileHandle::getNumberOfPages()
@@ -134,8 +182,8 @@ unsigned FileHandle::getNumberOfPages()
 
 RC FileHandle::collectCounterValues(unsigned &readPageCount, unsigned &writePageCount, unsigned &appendPageCount)
 {
-    readPageCount = readPageCounter;
-    writePageCount = writePageCounter;
-    appendPageCount = appendPageCounter;
+    readPageCount = this->readPageCounter;
+    writePageCount = this->writePageCounter;
+    appendPageCount = this->appendPageCounter;
     return 0;
 }
