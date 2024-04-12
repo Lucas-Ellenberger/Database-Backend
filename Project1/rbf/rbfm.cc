eec1b567b4cdf0d0c4c3d21588dc6ef5b7a4ecb8
Lucas Ellenberger <lmellenb@ucsc.edu>
2024-04-11 19:22:38 -0700

    progress on print record

--- Project1/rbf/rbfm.cc
+++ Project1/rbf/rbfm.cc
@@ -1,3 +1,8 @@
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <cmath>
+
 #include "rbfm.h"
 
 RecordBasedFileManager* RecordBasedFileManager::_rbf_manager = 0;
@@ -67,35 +72,59 @@ RC RecordBasedFileManager::readRecord(FileHandle &fileHandle, const vector<Attri
 }
 
 RC RecordBasedFileManager::printRecord(const vector<Attribute> &recordDescriptor, const void *data) {
-    if (_pf_manager == 0) {
-        cerr << "printRecord: _pf_manager has not been initialized.\n" << endl;
-        return 1;
-    }
-
-    num_records = recordDescriptor.size();
-    n = ceil(num_records / 8);
-    // Initialize a pointer to the first bit of the NULL bits.
-    null_ptr = data;
+    /* PagedFileManager *pfm = PagedFileManager::instance(); */
+    int nullBytes = ceil((double) recordDescriptor.size() / CHAR_BIT);
+    bool nullBit = false;
     // Initialize a pointer to the first byte of real data, which is after the n NULL bytes.
-    data_ptr = data + (n * sizeof(byte));
+    int dataOffset = nullBytes;
 
-    vector<Attribute>::iterator it;
-    int index = 0;
-    bool valid;
-    for (it=recordDescriptor.begin(); it!=recordDescriptor.end(); it++) {
+    // Initialize a pointer to the first bit of the NULL bits.
+    const char* nullOffset = (char *) data;
+
+    int byteOffset = 0;
+    int bitOffset = 7;
+    for (unsigned i = 0; i < recordDescriptor.size(); i++) {
+        cout << recordDescriptor[i].name << ":\t";
         // Check if the NULL bit is valid.
-        valid = (null_ptr & (1 << index))
-        if (recordDescriptor.type == 0) {  
-            cout << recordDescriptor.Name << ": " << 
-        } else if (recordDescriptor.type == 1) {
+        nullBit = (nullOffset[byteOffset] & (1 << bitOffset));
+        if (!nullBit) {
+            // The field in the record is either an int or float.
+            if (recordDescriptor[i].type == 0) {
+                int curr;
+                memcpy(&curr, data[dataOffset], sizeof(recordDescriptor[i].type));
+                dataOffset += recordDescriptor[i].length;
 
-        } else if (recordDescriptor.type == 2) {
+                cout << curr << "\t";
+            } else if (recordDescriptor[i].type == 1){
+                float curr;
+                memcpy(&curr, dataOffset, sizeof(recordDescriptor[i].type));
+                dataOffset += recordDescriptor[i].length;
 
+                cout << curr << "\t";
+            } else if (recordDescriptor[i].type == 2) {
+                int length;
+                memcpy(&length, dataOffset, sizeof(int));
+                dataOffset += sizeof(int);
+
+                char str[recordDescriptor[i].length];
+                memcpy(&str, dataOffset, recordDescriptor[i].length);
+                dataOffset += recordDescriptor[i].length;
+
+                cout << str << "\t";
+            } else {
+                cerr << endl << "printRecord: Invalid record descriptor type." << endl;
+                return 1;
+            }
         } else {
-            fprintf(stderr, "Invalid record descriptor type.\n");
-            return 1;
+            cout << "NULL\t";
         }
-        index++;
+
+        bitOffset--;
+        if (bitOffset < 0) {
+            bitOffset = 7;
+            byteOffset++;
+        }
     }
-    return -1;
+    cout << endl;
+    return 0;
 }
